0deffb319127b4ba2c3a38026303155dde3f73cc
Md Sajid Ansari <mdsajidalam8340@gmail.com>
2024-05-28 21:13:37 +0530

    centre homelisting

--- boxigo/src/App.jsx
+++ boxigo/src/App.jsx
@@ -4,7 +4,7 @@ import Home from "./page/Home";
 function App() {
   return (
     <>
-      <div>
+      <div className='max-w-[1200px] mx-auto flex gap-1 lg:px-0 px-5 justify-center'>
         <Home />
       </div>
     </>
