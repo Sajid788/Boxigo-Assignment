e98dce6ff5ca2927ae990c6a9ae5d69d3bc4ae9a
Md Sajid Ansari <mdsajidalam8340@gmail.com>
2024-05-29 14:46:47 +0530

    Implement sidebar

--- boxigo/src/App.jsx
+++ boxigo/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import SideBar from "./components/Sidebar";
 import Home from "./page/Home";
 
 function App() {
   return (
     <>
       <div className='max-w-[1200px] mx-auto flex gap-1 lg:px-0 px-5 justify-center'>
+        <SideBar/>
         <Home />
       </div>
     </>
